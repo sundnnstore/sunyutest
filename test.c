a1d3d4cf1b267777de99433196cfff169835eb12
Sunyuproject <3073350080@qq.com>
2023-08-23 14:11:46 +0800

    updated test.c

--- test.c
+++ test.c
@@ -4,10 +4,10 @@ int main()
     printf("hello world\n");
     printf("hello world\n");
     return 0;
-//D:\Sunyu\sunyutest>git config --global http.proxy http://127.0.0.1:7890
+//git config --global http.proxy http://127.0.0.1:7890
 
-//D:\Sunyu\sunyutest>git config --global git.proxy http://127.0.0.1:7890
+//git config --global git.proxy http://127.0.0.1:7890
 
-//D:\Sunyu\sunyutest>git config --global --unset https.proxy
+//git config --global --unset https.proxy
 
 }
